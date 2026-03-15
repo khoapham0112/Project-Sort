35bbbdf2ac8d516d7dd1181838e18292869e90b0
Yui-Okusora <dqdung3112@gmail.com>
2026-03-16 01:39:16 +0700

    sample

---
+++ Project-Sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if (argc > 7) return 1;
+
+    if (strcmp(argv[1], "-a") == 0)
+    {
+
+    }
+
+    return 0;
+}
