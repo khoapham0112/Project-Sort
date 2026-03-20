b1105871b0982db931f9ad2034b5112adb1423db
Koder <khoiloptruong@gmail.com>
2026-03-20 16:00:39 +0700

    change compare slection sort
    updated comparison counting.

--- SelectionSort.hpp
+++ SelectionSort.hpp
@@ -1,42 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <chrono>
-#include <random>
+#include <vector>
 
-void selectionSort(std::vector<int> &a, unsigned long long &compCount)
+std::pair<double, double> InsertionSort(int a[], int n)
 {
-    int n = a.size();
-    compCount = 0;
-
-    for (int i = 0; i < n - 1; ++i)
-    {
-        int min_idx = i;
-
-        for (int j = i + 1; j < n; ++j)
-        {
-            compCount++; 
-            if (a[j] < a[min_idx])
-            {
-                min_idx = j;
-            }
-        }
-        if (min_idx != i)
-        {
-            std::swap(a[i], a[min_idx]);
-        }
-    }
-}
-
-std::pair<long long, double> runSelectionSort(std::vector<int> data)
-{
-    unsigned long long comparisons = 0;
+    double compare = 0;
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    selectionSort(data, comparisons);
+    for (int i = 1; i < n; i++)
+    {
+        compare++;
+        int key = a[i];
+        int j = i - 1;
+
+        while (j >= 0)
+        {
+            compare++;
+
+            if (a[j] > key)
+            {
+                compare++;
+                a[j + 1] = a[j];
+                j--;
+            }
+            else
+                break;
+        }
+
+        a[j + 1] = key;
+    }
 
     auto end = std::chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
 
-    return { comparisons, duration.count() };
+    std::chrono::milliseconds duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    return { compare, duration.count() };
 }
